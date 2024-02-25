0b14e744556a2d070f7fce5ea75624d59321fb83
emrah karaman <ekaraman89@hotmail.com>
2024-02-25 20:57:48 +0300

    net 8

--- MediatrBehavior/DbContext.cs
+++ MediatrBehavior/DbContext.cs
@@ -2,15 +2,16 @@
 {
 	public class DbContext
 	{
-		private readonly List<UserEntity> _users = new()
-		{
-			new () { Id = Guid.NewGuid(), Name ="user1", Email="user1@mail.com"},
-			new () { Id = Guid.NewGuid(), Name ="user2", Email="user2@mail.com"},
-			new () { Id = Guid.NewGuid(), Name ="user3", Email="user3@mail.com"},
-			new () { Id = Guid.NewGuid(), Name ="user4", Email="user4@mail.com"},
-			new () { Id = Guid.NewGuid(), Name ="user5", Email="user5@mail.com"},
-		};
+		private readonly List<UserEntity> _users =
+		[
+			new UserEntity { Id = Guid.NewGuid(), Name = "user1", Email = "user1@mail.com" },
+			new UserEntity { Id = Guid.NewGuid(), Name = "user2", Email = "user2@mail.com" },
+			new UserEntity { Id = Guid.NewGuid(), Name = "user3", Email = "user3@mail.com" },
+			new UserEntity { Id = Guid.NewGuid(), Name = "user4", Email = "user4@mail.com" },
+			new UserEntity { Id = Guid.NewGuid(), Name = "user5", Email = "user5@mail.com" }
+		];
 
+		
 		public UserEntity AddUser(UserEntity user)
 		{
 			_users.Add(user);
